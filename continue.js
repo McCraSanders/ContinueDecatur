a39c9e632740157563cd22dc8a2e8c8c843f2957
McCrae Sanders <mccraebsanders@gmail.com>
2017-09-11 14:37:59 -0400

    Update continue.js

--- continue.js
+++ continue.js
@@ -1,14 +1,39 @@
-// Safari Sniffing for the purpose of using mix-blend-mode
-// Not default in chrome / funky in FF
+(function(){
+	var fullSVG = document.getElementById('burger'),
+		bg = document.getElementById('main-container'),
+		topBun = document.getElementById('top-bun'),
+		lettuce = document.getElementById('lettuce'),
+		beef = document.getElementById('beef'),
+		bottomBun = document.getElementById('bottom-bun'),
+		stillCheese = document.getElementById('still-cheese'),
+		seeds = document.getElementById('seed-group'),
+		tomatoes = document.getElementsByClassName('tomatoes'),
+		meltingCheese = document.getElementsByClassName('melting-cheese'),
+		elements = document.getElementsByClassName('element');
 
-(function() {
-  function isSafari() {
-    var userAgent = navigator.userAgent;
-    return (/Safari/gi).test(userAgent) &&
-      !(/Chrome/gi).test(userAgent);
-  }
-  if(isSafari()) {
-    var overlay = document.querySelector('.overlay');
-    overlay.classList.add('multiply');
-  }
+		TweenMax.set(meltingCheese, { y:"-30%", opacity: 0});
+
+		startItems = new TimelineLite();
+		startItems.staggerFromTo('.item', 0.6 , {width: '50%', x: "50%", y: "-500%", opacity: 1, scale: 0, ease: Bounce.easeIn}, {width: '100%', x: "0%", y: "0%", scale: 1, ease: Bounce.easeOut }, 0.1)
+		.staggerFrom(".seeds", 0.1, {x: 5, opacity: 1, scale: 0, ease: Power0.easeNone, y: -500}, 0.1)
+		.to(".seeds", 0.1, {y: "0%"})
+		.to(topBun, 0.1 ,{y: "-100%", onStart: throwSeeds})
+		.to(lettuce, 0.1 ,{y: "-40%"})
+		.to(topBun, 0.1 ,{y: "-0%", onComplete: squeeze})
+
+		TweenMax.fromTo(meltingCheese, 1, { y:"-50%", opacity: 0}, {height: "120%", y: "-8%", opacity: 1, ease:Expo.easeOut, delay: 0.6 } );
+
+		function squeeze() {
+			TweenMax.to(lettuce, 0.1 ,{y: "0%"});
+			TweenMax.fromTo(beef, 0.5, {x: "-3%", scale: 1.05}, {x: "0%", scale: 1});
+			TweenMax.fromTo(topBun, 0.5, {x: "-3%", scale: 1.05}, {x: "0%", scale: 1});
+		}
+
+		function throwSeeds(){
+			var seedToss = 0.2;
+			TweenMax.staggerTo(".seeds", seedToss, {scale: 1.1,  x: "10", ease: Elastic.easeNone, y: -300});
+			TweenMax.staggerTo(".seeds", seedToss, {scale: 1, x: "0", delay: seedToss, ease: Elastic.easeNone, y: 0});
+		}
+
+
 })();
